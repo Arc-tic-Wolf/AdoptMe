44ff8ca2b242dd9a1c2dc83d8779d0b60a00fc48
Aninda <aninda.messy@gmail.com>
2020-11-17 19:57:58 +0600

    Implementation by pure React done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-// Your code is going to go here
+
 const App = () => {
     return React.createElement(
-      "div",
-      {},
-      React.createElement("h1", {}, "Adopt"),
-      React.createElement("h1", {}, "Me!")
+        "div",
+        {},
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {name:"Lucy",animal:"dog"} ),
+        React.createElement(Pet, {name:"Tom",animal:"cat"} ),
+        React.createElement(Pet, {name:"Pheonix",animal:"bird"} ),
+        
+        
     );
-  };
+};
 
-  ReactDOM.render(React.createElement(App), document.getElementById("root"));
+const Pet = props =>{
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h2",{},props.name),
+        React.createElement("h3",{},props.animal)
+    );
+}
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
